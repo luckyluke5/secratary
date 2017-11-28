03077c2f462f2e79a93ac795d8df7cb1c53e423c
luckyluke5 <33432762+luckyluke5@users.noreply.github.com>
2017-11-28 20:57:18 +0100

    Hinzufügen der section klasse
    voräufig gute version der section klasse

---
+++ section.h
@@ -0,0 +1,48 @@
+/*
+ * section.h
+ * 
+ * Copyright 2017 Lucas <lucas@linux-n51u>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#ifndef SECTION_H
+#define SECTION_H
+
+#include "questionStrategieManager.h"
+#include "answerManager.h"
+
+class Section
+{
+	public:
+		Section(DataController & _dataController);
+		virtual ~Section();
+
+		void run();
+	
+	private:
+		QuestionStrategieManager qsManager;
+		AnswerManager aManager;
+
+		DataController dataController;
+
+		/* add your private declarations */
+};
+
+#endif /* SECTION_H */ 
