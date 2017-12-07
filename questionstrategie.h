b4e879e7ca6e26d55235672e28ba1d1aa8f2a857
luckyluke5 <33432762+luckyluke5@users.noreply.github.com>
2017-12-07 16:51:47 +0100

    hinzufügen der klasse questionstrategie

---
+++ questionstrategie.h
@@ -0,0 +1,42 @@
+/*
+ * questionstrategie.h
+ * 
+ * Copyright 2017 Lucas <lucas@linux-n51u>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#ifndef QUESTIONSTRATEGIE_H
+#define QUESTIONSTRATEGIE_H
+
+class QuestionStrategie
+{
+	public:
+		QuestionStrategie();
+		virtual ~QuestionStrategie();
+
+		QuestionID actuallQuestionID();
+
+		std::vector<QuestionID> questionIDs;
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* QUESTIONSTRATEGIE_H */ 
